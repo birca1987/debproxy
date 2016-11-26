009c4d4a11f4b73861634314d50e251f9ef1b0cf
sybdata <as15@wt.de>
2016-11-26 22:47:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,22 +10,22 @@ do_copy_config()
     DEST=.
   fi
 
-  DEST=/home/aceproxy/aceproxy/$DEST/$CONFIG
+  DEST=/home/tv/aceproxy-master/$DEST/$CONFIG
 
   if [[ ! -f $SRC ]]; then
     return 0
   fi
 
-  install -m 0644 -o aceproxy $SRC $DEST
+  install -m 0644 -o tv $SRC $DEST
 }
 
-adjust_settings()
-{
-  sed -i 's/acespawn = False/acespawn = True/' /home/aceproxy/aceproxy/aceconfig.py
-  sed -i 's/vlcuse = False/vlcuse = True/' /home/aceproxy/aceproxy/aceconfig.py
-  sed -i 's/vlcspawn = False/vlcspawn = True/' /home/aceproxy/aceproxy/aceconfig.py
-  sed -i 's/videoobey = True/videoobey = False/' /home/aceproxy/aceproxy/aceconfig.py
-}
+#adjust_settings()
+#{
+#  sed -i 's/acespawn = False/acespawn = True/' /home/tv/aceproxy-master/aceconfig.py
+#  sed -i 's/vlcuse = False/vlcuse = True/' /home/tv/aceproxy-master/aceconfig.py
+#  sed -i 's/vlcspawn = False/vlcspawn = True/' /home/tv/aceproxy-master/aceconfig.py
+#  sed -i 's/videoobey = True/videoobey = False/' /home/tv/aceproxy-master/aceconfig.py
+#}
 
 do_copy_config aceconfig.py
 do_copy_config p2pproxy.py     plugins/config
@@ -33,6 +33,6 @@ do_copy_config torrenttelik.py plugins/config
 do_copy_config torrenttv.py    plugins/config
 do_copy_config allfon.py       plugins/config
 
-adjust_settings
+#adjust_settings
 
 exec /usr/bin/supervisord
