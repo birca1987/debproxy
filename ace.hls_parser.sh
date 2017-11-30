c53251409badc959830ddb83112a468adaf04f31
birca1987 <birca_gheorghe@yahoo.com>
2017-11-30 21:29:42 +0000

    Update ace.hls_parser.sh

--- ace.hls_parser.sh
+++ ace.hls_parser.sh
@@ -5,14 +5,15 @@ parser="/usr/bin/jq"
 filepath="/mnt/media/playlists"
 
 playlist_url="http://pomoyka.win/trash/ttv-list/ttv.json"
-json="$filepath/m3u/ttv.json"
+json="$filepath/m3u/as.json"
 outdir="$filepath/m3u"
 
 header="#EXTM3U"
 newline="ZFZFaZZZ"
-informational="informational"
+pron="Эротика"
 
-url_prefix="http://127.0.0.1:6878/ace/manifest.m3u8?id="
+url_prefix="http://192.168.0.10:6878/ace/getstream?id="
+url_prefix1="http://192.168.0.10:6878/ace/manifest.m3u8?id="
 url_postfix=""
 
 mkdir -p $filepath >> /dev/null 2>&1
@@ -26,9 +27,12 @@ size=`echo $data |$parser  '.channels|length'`
 
 if [ $size -gt 1 ]
 then
-  param="'.channels|=sort_by(.cat,.name)|.channels[]|select(.cat = \"$informational\")|\"#EXTINF:-1 group-title=\\\"\"+.cat+\"\\\", \"+.name+\"$newline\"+\"$url_prefix\"+.url+\"$url_postfix\"'|sed 's/$newline/\n/g'"
-  eval "echo \$header; echo \$data | " $parser "-r" $param > $outdir/as.kodi.m3u
+  param="'.channels|=sort_by(.cat,.name)|.channels[]|select(.cat = \"$pron\")|\"#EXTINF:-1 group-title=\\\"\"+.cat+\"\\\", \"+.name+\"$newline\"+\"$url_prefix\"+.url+\"$url_postfix\"'|sed 's/$newline/\n/g'"
+  eval "echo \$header; echo \$data | " $parser "-r" $param > $outdir/kodi.m3u
+  
+  param="'.channels|=sort_by(.cat,.name)|.channels[]|select(.cat = \"$pron\")|\"#EXTINF:-1 group-title=\\\"\"+.cat+\"\\\", \"+.name+\"$newline\"+\"$url_prefix1\"+.url+\"$url_postfix\"'|sed 's/$newline/\n/g'"
+  eval "echo \$header; echo \$data | " $parser "-r" $param > $outdir/kod1.m3u
 
-  param="'.channels|=sort_by(.cat,.name)|.channels[]|select(.cat = \"$informational\")|\"#EXTINF:-1, \"+.name+\" (\"+.cat+\")$newline\"+\"$url_prefix\"+.url+\"$url_postfix\"'|sed 's/$newline/\n/g'"
-  eval "echo \$header; echo \$data | " $parser "-r" $param > $outdir/as.m3u
+  param="'.channels|=sort_by(.cat,.name)|.channels[]|select(.cat = \"$pron\")|\"#EXTINF:-1, \"+.name+\" (\"+.cat+\")$newline\"+\"$url_prefix\"+.url+\"$url_postfix\"'|sed 's/$newline/\n/g'"
+  eval "echo \$header; echo \$data | " $parser "-r" $param > $outdir/ttv.m3u
 fi
